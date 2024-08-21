fe67d81ea149ed392a082109aaaed8c0e57073ed
AshwiniRathod13 <162275313+AshwiniRathod13@users.noreply.github.com>
2024-08-21 09:58:39 +0530

    connecting mongodb using mongoose

--- Server.js
+++ Server.js
@@ -1,11 +1,12 @@
  import express from 'express';
 import { products } from './src/constants/Constants.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config()
  const port = process.env.PORT || 5001;
  const app = express();
-
+ connectDB();
 
  app.get('/',(req, res) =>{
     res.send('App is running');
